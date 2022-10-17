9a7c9b4ea6ea6443549e8e2cbcd40fefaeea6739
diana-atas <dianadavidatas@gmail.com>
2022-10-17 16:06:09 +1300

    integrate mui with nextjs

---
+++ utils/theme.js
@@ -0,0 +1,11 @@
+// overrides MUI's default theme settings
+
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fcba03',
+    },
+  },
+})
