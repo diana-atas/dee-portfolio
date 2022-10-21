06031f5035a93af6b472b57caee93f0640c9a284
diana-atas <dianadavidatas@gmail.com>
2022-10-21 16:17:31 +1300

    delete background and minor clean-up

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,14 @@
-import { Container, Box, Grid, Typography, Paper } from '@mui/material'
+import { Container, Box, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
-// import bg1 from '../public/images/hero-bg/bg1.png'
-// import bg2 from '../public/images/hero-bg/bg2.png'
-// import bg3 from '../public/images/hero-bg/bg3.png'
-// import bg4 from '../public/images/hero-bg/bg4.png'
-
-const HeroLayout = styled('section')(({ theme }) => ({
+const HeroLayout = styled('div')(({ theme }) => ({
   color: theme.palette.common.white,
   position: 'relative',
   display: 'flex',
   alignItems: 'center',
+  // height: '100%',
+  // width: '100%',
   [theme.breakpoints.up('sm')]: {
     height: '80vh',
     minHeight: 500,
@@ -18,37 +16,30 @@ const HeroLayout = styled('section')(({ theme }) => ({
   },
 }))
 
-const Background = styled(Box)(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundImage: 'url(/images/hero-bg/bg1.png)',
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-}))
-
 const Hero = () => {
+  let parallax
   return (
-    <HeroLayout>
-      <Container
-      // sx={{
-      //   mt:3,
-
-      // }}
-      >
-        <Background>
+    <Parallax>
+      <HeroLayout>
+        <Container
+        // sx={{
+        //   mt: 3,
+        //   mb: 14,
+        //   display: 'flex',
+        //   flexDirection: 'column',
+        //   alignItems: 'center',
+        // }}
+        >
+          <ParallaxLayer></ParallaxLayer>
           <Typography variant="h1" component="h1">
             D Atas
           </Typography>
           <Typography variant="h2" component="h2">
             Full Stack Developer
           </Typography>
-        </Background>
-      </Container>
-    </HeroLayout>
+        </Container>
+      </HeroLayout>
+    </Parallax>
   )
 }
 
