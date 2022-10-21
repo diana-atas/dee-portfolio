65c5bcf0580521568e64a3c72a783c7caf062771
diana-atas <dianadavidatas@gmail.com>
2022-10-21 16:52:45 +1300

    add moving bg1

--- components/Hero.js
+++ components/Hero.js
@@ -15,24 +15,44 @@ import { styled } from '@mui/material/styles'
 //   },
 // }))
 
+const ScrollingBg1 = styled('div')({
+  '@keyframes slide': {
+    '0%': {
+      transform: 'translate(0,0)',
+    },
+    '100%': {
+      transform: 'translate(0,-4096px)',
+    },
+  },
+  background: 'url(/images/hero-bg/bg1.png) repeat-y',
+  height: 8192,
+  width: 2048,
+  animation: `slide 10s linear infinite`,
+})
+
 const Hero = () => {
   return (
     // <HeroLayout>
     <Container
-    // sx={{
-    //   mt: 3,
-    //   mb: 14,
-    //   display: 'flex',
-    //   flexDirection: 'column',
-    //   alignItems: 'center',
-    // }}
+      sx={{ overflow: 'hidden', height: '100vh' }}
+      // sx={{
+      //   mt: 3,
+      //   mb: 14,
+      //   display: 'flex',
+      //   flexDirection: 'column',
+      //   alignItems: 'center',
+      // }}
     >
-      <Typography variant="h1" component="h1">
-        D Atas
-      </Typography>
-      <Typography variant="h2" component="h2">
-        Full Stack Developer
-      </Typography>
+      <ScrollingBg1>
+        <Box sx={{ position: 'absolute' }}>
+          <Typography variant="h1" component="h1">
+            D Atas
+          </Typography>
+          <Typography variant="h2" component="h2">
+            Full Stack Developer
+          </Typography>
+        </Box>
+      </ScrollingBg1>
     </Container>
     // </HeroLayout>
   )
