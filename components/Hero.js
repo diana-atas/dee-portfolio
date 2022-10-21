acd582fe2493c82858f48ddff066dd9a54056027
diana-atas <dianadavidatas@gmail.com>
2022-10-21 20:15:54 +1300

    fix position for layout background container

--- components/Hero.js
+++ components/Hero.js
@@ -1,21 +1,21 @@
 import { Container, Box, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-// const HeroLayout = styled('div')(({ theme }) => ({
-//   color: theme.palette.common.white,
-//   position: 'relative',
-//   display: 'flex',
-//   alignItems: 'center',
-//   // height: '100%',
-//   // width: '100%',
-//   [theme.breakpoints.up('sm')]: {
-//     height: '80vh',
-//     minHeight: 500,
-//     maxHeight: 1300,
-//   },
-// }))
+const HeroLayout = styled('div')(({ theme }) => ({
+  color: theme.palette.common.white,
+  // position: 'relative',
+  // display: 'flex',
+  // alignItems: 'center',
+  // height: '100%',
+  // width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    height: '80vh',
+    minHeight: 500,
+    maxHeight: 1300,
+  },
+}))
 
-const ScrollingBg1 = styled('div')({
+const ScrollingBg = styled('div')({
   '@keyframes slide': {
     '0%': {
       transform: 'translate(0,0)',
@@ -24,37 +24,71 @@ const ScrollingBg1 = styled('div')({
       transform: 'translate(0,-4096px)',
     },
   },
-  background: 'url(/images/hero-bg/bg1.png) repeat-y',
+  position: 'absolute',
   height: 8192,
   width: 2048,
-  animation: `slide 10s linear infinite`,
 })
 
 const Hero = () => {
   return (
-    // <HeroLayout>
-    <Container
-      sx={{ overflow: 'hidden', height: '100vh' }}
-      // sx={{
-      //   mt: 3,
-      //   mb: 14,
-      //   display: 'flex',
-      //   flexDirection: 'column',
-      //   alignItems: 'center',
-      // }}
-    >
-      <ScrollingBg1>
-        <Box sx={{ position: 'absolute' }}>
-          <Typography variant="h1" component="h1">
-            D Atas
-          </Typography>
-          <Typography variant="h2" component="h2">
-            Full Stack Developer
-          </Typography>
-        </Box>
-      </ScrollingBg1>
-    </Container>
-    // </HeroLayout>
+    <HeroLayout>
+      <Box
+        sx={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          zIndex: '99',
+        }}
+      >
+        <Typography variant="h1" component="h1" align="center">
+          Dee Atas
+        </Typography>
+        <Typography variant="h2" component="h2" align="center">
+          Full Stack Developer
+        </Typography>
+      </Box>
+
+      <Container
+        sx={{
+          overflow: 'hidden',
+          height: '100vh',
+          position: 'relative',
+        }}
+      >
+        <ScrollingBg
+          sx={{
+            background: 'url(/images/hero-bg/bg1.png) repeat-y',
+            animation: `slide 100s linear infinite`,
+            zIndex: '-1',
+          }}
+        />
+        <ScrollingBg
+          sx={{
+            background: 'url(/images/hero-bg/bg2.png) repeat-y',
+            animation: `slide 70s linear infinite`,
+            zIndex: '1',
+          }}
+        />
+        <ScrollingBg
+          sx={{
+            background: 'url(/images/hero-bg/bg3.png) repeat-y',
+            animation: `slide 70s linear infinite`,
+            zIndex: '2',
+          }}
+        />
+        <ScrollingBg
+          sx={{
+            background: 'url(/images/hero-bg/bg4.png) repeat-y',
+            animation: `slide 100s linear infinite`,
+            zIndex: '3',
+          }}
+        />
+      </Container>
+    </HeroLayout>
   )
 }
 
