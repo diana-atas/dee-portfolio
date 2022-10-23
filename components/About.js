8aa5fdc83335036a5531e55179411359971238fa
diana-atas <dianadavidatas@gmail.com>
2022-10-24 12:21:56 +1300

    create about component

---
+++ components/About.js
@@ -0,0 +1,28 @@
+import { Container, Box, Typography } from '@mui/material'
+
+const About = () => {
+  return (
+    <Container>
+      <Box>
+        <Typography component="h2" variant="h2"></Typography>
+        About
+        <Typography variant="body1" align="justify">
+          After a decade of data processing and number crunching in the employee
+          insurance industry using Excel, I sought to expand my problem-solving
+          and analytical skills further. I have ventured into building software
+          that is meaningful, artistic, and accessible, and never turned back. I
+          love to learn. I appreciate knowledge and the value of having to
+          collaborate and share that with others.
+        </Typography>
+        <Typography variant="body1" align="justify">
+          When I'm not coding, I have an assortment of activities on different
+          days to keep fit: running, road cycling, a bit of yoga or some
+          bouldering. My family and I are also snowboarding amateurs and love to
+          play in the snow whenever we get the chance.
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
+
+export default About
