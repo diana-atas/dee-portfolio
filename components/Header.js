ada1f9513181ab50f449a6c7b156d9023c063fa4
diana-atas <dianadavidatas@gmail.com>
2022-10-27 12:30:39 +1300

    transition in progress

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,29 @@
 import { Box, Stack } from '@mui/material'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Header = () => {
+  const router = useRouter()
+  const aniStart = () => {
+    console.log('Animation Start')
+  }
+  const aniEnd = () => {
+    console.log('Animation End')
+  }
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', aniStart)
+    router.events.on('routeChangeComplete', aniEnd)
+    router.events.on('routeChangeError', aniEnd)
+
+    return () => {
+      router.events.off('routeChangeStart', aniStart)
+      router.events.off('routeChangeComplete', aniEnd)
+      router.events.off('routeChangeError', aniEnd)
+    }
+  }, [router])
+
   return (
     <Box
       sx={{
