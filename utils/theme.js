4539d14c028597eabf13552edc6652a4ec50dce6
diana-atas <dianadavidatas@gmail.com>
2022-10-30 12:32:35 +1300

    remove responsiveFontSizes

--- utils/theme.js
+++ utils/theme.js
@@ -1,14 +1,13 @@
 // overrides MUI's default theme settings
 
-import { createTheme, responsiveFontSizes } from '@mui/material'
+import { createTheme } from '@mui/material'
 
-export let theme = createTheme({
+export const theme = createTheme({
   palette: {
     primary: {
       main: '#fcba03',
-      mode: 'dark',
     },
   },
 })
 
-theme = responsiveFontSizes(theme)
+// theme = responsiveFontSizes(theme)
