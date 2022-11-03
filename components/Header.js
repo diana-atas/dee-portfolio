58e6386c8defc47286686a8d60ba51bcf6e64197
diana-atas <dianadavidatas@gmail.com>
2022-11-04 05:45:13 +1300

    change nav text to icons

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,11 @@ import { useEffect } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+  faHouse,
+  faCircleInfo,
+  faRocket,
+} from '@fortawesome/free-solid-svg-icons'
 
 const NavStyles = styled(AppBar)(({ theme }) => ({
   color: theme.palette.common.white,
@@ -14,7 +19,15 @@ const NavStyles = styled(AppBar)(({ theme }) => ({
   zIndex: '99',
   fontFamily: 'Exo',
   textTransform: 'uppercase',
+  display: 'flex',
+  justifyContent: 'center',
 }))
+
+const iconStyle = {
+  // border: '1px solid #fff',
+  // borderRadius: '0.25em',
+  padding: '0.25em',
+}
 
 const Header = () => {
   const router = useRouter()
@@ -40,17 +53,44 @@ const Header = () => {
   return (
     <NavStyles>
       <Toolbar disableGutters>
-        <Stack direction="row" spacing={2} flexGrow={1} ml={3}>
-          <Link href="/">Home</Link>
-          <Link href="portfolio">CV</Link>
-          <Link href="#">Projects</Link>
+        <Stack direction="row" spacing={1} flexGrow={1} ml={2}>
+          <Link href="/">
+            {/* <FontAwesomeIcon icon={faHouse} style={iconStyle} fixedWidth /> */}
+            <FontAwesomeIcon icon={faHouse} size="lg" cursor="pointer" />
+          </Link>
+          <Link href="portfolio">
+            <FontAwesomeIcon
+              icon={faCircleInfo}
+              size="lg"
+              cursor="pointer"
+              title="View my CV"
+            />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon
+              icon={faRocket}
+              size="lg"
+              cursor="pointer"
+              title="See what I'm working on"
+            />
+          </Link>
         </Stack>
-        <Stack direction="row" spacing={2} justifyContent="flex-end" mr={3}>
+        <Stack direction="row" spacing={1} justifyContent="flex-end" mr={2}>
           <a href="https://www.linkedin.com/in/diana-atas" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            {/* <FontAwesomeIcon icon={faLinkedin} fixedWidth /> */}
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="xl"
+              title="Connect with me on LinkedIn"
+            />
           </a>
           <a href="https://github.com/diana-atas" target="_blank">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
+            {/* <FontAwesomeIcon icon={faGithub} fixedWidth /> */}
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="xl"
+              title="Follow me on GitHub"
+            />
           </a>
         </Stack>
       </Toolbar>
